43832630380bdfe2d02404d3f13eb6e161a1e862
Milinbhakta <desire2dosomething@gmail.com>
2024-02-21 11:52:26 -0400

    Delete unused files and update dependencies

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '../app.postcss';
+</script>
+
+<header class="header-bar">
+	<div class="container">
+		<h1 class="h2 ml-3 p-3 capitalize">
+			<span class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">
+				Desire Chat
+			</span>
+		</h1>
+	</div>
+</header>
+<slot class="container" />
+
+<style>
+	.header-bar {
+		width: 100%;
+		background: rgba(0, 0, 0, 0.1);
+		backdrop-filter: blur(10px);
+		display: flex;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	let username: string = '';
+
+	function handleInputChange(event: Event) {
+		const target = event.target as HTMLInputElement;
+		username = target.value;
+	}
+</script>
+
+<main class="container mx-auto mt-40">
+	<div class="card variant-ghost">
+		<div class="card-header">
+			<h2 class="h2">Login</h2>
+		</div>
+		<div class="p-5">
+			<label class="label">
+				<span>Username</span>
+				<input
+					id="username"
+					class="input"
+					type="text"
+					placeholder="Your Full Name"
+					bind:value={username}
+					on:change={handleInputChange}
+				/>
+			</label>
+		</div>
+		<div class="card-footer">
+			<button type="button" class="btn btn-primary variant-filled">Login</button>
+		</div>
+	</div>
+</main>
