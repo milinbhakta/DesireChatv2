fee6f096fa87e80c2f0a67fe142715bc017ba0f2
milinbhakta <desire2dosomething@gmail.com>
2024-02-22 09:36:13 -0400

    Refactor code for improved performance

---
+++ packages/server/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get<{}, any>("/", (req, res) => {
+  res.json({
+    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+  });
+});
+
+export default app;
