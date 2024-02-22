e1d4afa9e5be20dd3a8d74b8966c948722715810
milinbhakta <desire2dosomething@gmail.com>
2024-02-22 12:32:05 -0400

    Update CSS and dependencies, add chat functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import "./App.css";
+import { StreamChat, User } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
 
-function App() {
-  return (
-    <>
-      <div>APP</div>
-    </>
-  );
-}
+import "stream-chat-react/dist/css/v2/index.css";
+
+const userId = "solitary-violet-5";
+const userName = "solitary-violet-5";
+
+const user: User = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?id=${userId}&name=${userName}`,
+};
+
+const apiKey = "3umjzmz8k42g";
+const userToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic29saXRhcnktdmlvbGV0LTUifQ.Y08OHnM7mS50WGIeYO8X_KaWGKxDVWca8c0oa0fOz-I";
+
+const chatClient = new StreamChat(apiKey);
+chatClient.connectUser(user, userToken);
+
+const channel = chatClient.channel("messaging", "custom_channel_id", {
+  // add as many custom fields as you'd like
+  image: "https://www.drupal.org/files/project-images/react.png",
+  name: "Talk about React",
+  members: [userId],
+});
+
+const App = () => (
+  <Chat client={chatClient} theme="str-chat__theme-light">
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
